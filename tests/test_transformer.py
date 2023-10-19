cbde540289b995aa61816ba458e50241b6d81eee
MoooCat <141886018+MooooCat@users.noreply.github.com>
2023-10-19 18:03:41 +0800

    Update test_transformer.py

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -10,6 +10,7 @@ sys.path.append(os.getcwd())
 
 from sdgx.transform.transformer import DataTransformerCTGAN
 from sdgx.utils.io.csv_utils import *
+from sdgx.transform.transformer_opt import DataTransformer
 
 def test_transformer_original():
     demo_data, discrete_cols = get_demo_single_table()
@@ -17,9 +18,14 @@ def test_transformer_original():
     ctgan_transformer.fit(demo_data, discrete_cols)
     transformed_data = ctgan_transformer.transform(demo_data)
 
-def test_transformer():
+def test_transformer_opt():
     # 测试经过内存优化之后的 transformer 
     demo_data, discrete_cols = get_demo_single_table()
+    demo_data_path = "./dataset/adult.csv"
+    opt_transformer = DataTransformer()
+    opt_transformer.fit(demo_data_path, discrete_cols)
+    output_path = "tmp.csv"
+    opt_data_path = opt_transformer.transform(demo_data_path)
     
     pass
 
