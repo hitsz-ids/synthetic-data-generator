e03043dc8ffeae4671797544de7876fff83c4ee2
MooooCat <gyz@guoyunzhe.com>
2023-12-08 18:19:26 +0800

    Update column metric base class

--- sdgx/metrics/column/base.py
+++ sdgx/metrics/column/base.py
@@ -1,6 +1,6 @@
+import pandas as pd 
 
-
-class columnMetric():
+class columnMetric(object):
 
     upper_bound = None
     lower_bound = None
@@ -12,13 +12,38 @@ class columnMetric():
     
     @classmethod
     def check_input(real_data, synthetic_data):
-        # should be list \ Series \ 1d-Array
+        # Input parameter must not contain None value
+        if real_data is None or synthetic_data is None:
+           raise TypeError("Input contains None.") 
 
-        pass
+        # The data type should be same 
+        if type(real_data) is not type(synthetic_data):
+            raise TypeError("Data type of real_data and synthetic data should be the same.")
+
+        # Check some data-types that must not be allowed
+        if type(real_data) in [int, float, str]:
+            raise TypeError('real_data\'s type must not be None, int, float or str')
+        
+        # if type is pd.Series, return directly
+        if type(real_data) is pd.Series:
+            return real_data, synthetic_data
+        
+        # if type is not pd.Series, tranfer it to Series
+        try:
+            real_data = pd.Series(real_data)
+            synthetic_data = pd.Series(synthetic_data)
+            return real_data, synthetic_data
+        except:
+            raise TypeError('An error occurred while converting to pd.Series')
+        finally:
+            return None, None
 
     @classmethod
     def calculate(real_data, synthetic_data):
-
+        # This method should first check the input
+        # such as: 
+        real_data, synthetic_data = columnMetric.check_input(real_data, synthetic_data)
+        
         raise NotImplementedError()
 
     @classmethod
