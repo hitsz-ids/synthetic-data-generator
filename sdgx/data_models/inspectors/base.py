d8fa8b9cfadf520af0b6cecd8465708a2b4d3529
Wh1isper <9573586@qq.com>
2023-12-14 15:07:58 +0800

    Some framework on metadata

--- sdgx/data_models/inspectors/base.py
+++ sdgx/data_models/inspectors/base.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from sdgx.data_models.metadata import Metadata
+
 
 class Inspector:
     """
@@ -7,6 +9,9 @@ class Inspector:
 
     Inspector is used to inspect data and generate metadata automatically.
     """
+
+    ready: bool
+    """Ready to inspect, maybe all fields are fitted."""
 
     def fit(self, raw_data: pd.DataFrame):
         """Fit the inspector.
@@ -16,5 +21,5 @@ class Inspector:
         """
         return
 
-    def inspect(self):
+    def inspect(self) -> Metadata:
         """Inspect raw data and generate metadata."""
--- sdgx/data_models/metadata.py
+++ sdgx/data_models/metadata.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
 from enum import Enum
-from typing import List
 
 import pandas as pd
 from pydantic import BaseModel
 
 from sdgx.data_loader import DataLoader
+from sdgx.data_models.inspectors.manager import InspectorManager
 
 
 class DType(Enum):
@@ -23,10 +23,33 @@ class Relationship:
 class Metadata(BaseModel):
     # fields: List[str]
 
-    @classmethod
-    def from_dataloader(dataloader: DataLoader) -> "Metadata":
-        return Metadata()
+    def update(self, metadata: Metadata):
+        return self
 
     @classmethod
-    def from_dataframe(df: pd.DataFrame) -> "Metadata":
-        return Metadata()
+    def from_dataloader(cls, dataloader: DataLoader, max_chunk: int = 10) -> "Metadata":
+        inspectors = InspectorManager().init_all_inspectors()
+        for i, chunk in enumerate(dataloader.iter()):
+            for inspector in inspectors:
+                inspector.fit(chunk)
+            if all(i.ready for i in inspectors) or i > max_chunk:
+                break
+
+        metadata = Metadata()
+        for inspector in inspectors:
+            metadata.update(inspector.inspect())
+
+        return metadata
+
+    @classmethod
+    def from_dataframe(cls, df: pd.DataFrame) -> "Metadata":
+        inspectors = InspectorManager().init_all_inspectors()
+        for inspector in inspectors:
+            inspector.fit(df)
+
+        metadata = Metadata()
+        for inspector in inspectors:
+            if inspector.ready:
+                metadata.update(inspector.inspect())
+
+        return metadata
