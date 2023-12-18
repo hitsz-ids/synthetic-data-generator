7b66566453111347fcfdd9c9d630d6661bce103d
MoooCat <141886018+MooooCat@users.noreply.github.com>
2023-12-18 16:43:27 +0800

    Update Base Class of Metric (#60)
    * Update metric base-class
    
    * Update column metric base class
    
    * Update Metric Base Class
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Apply suggestions from code review
    
    Co-authored-by: Zhongsheng Ji <9573586@qq.com>
    
    * Feature: Add metric jsd (#66) (#71)
    
    * Feature metric jsd (#66)
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Apply suggestions from code review
    
    Co-authored-by: Zhongsheng Ji <9573586@qq.com>
    
    * jsd
    
    * jsd
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * base更新
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Delete .idea directory
    
    ---------
    
    
    Co-authored-by: Jinhang Su <171846802@qq.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: MoooCat <141886018+MooooCat@users.noreply.github.com>
    Co-authored-by: Zhongsheng Ji <9573586@qq.com>
    
    
    * Apply suggestions from code review
    
    Co-authored-by: Zhongsheng Ji <9573586@qq.com>
    
    * Update type hint and comments.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Apply suggestions from code review
    
    Co-authored-by: Zhongsheng Ji <9573586@qq.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    
    Co-authored-by: Jinhang Su <171846802@qq.com>
    Co-authored-by: sjh120 <171846802@qq.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Zhongsheng Ji <9573586@qq.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix typo error
    
    unnecessary imports are also removed.
    
    * Add type hints and comments
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update input check methods
    
    The format of some codes has also been adjusted.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Zhongsheng Ji <9573586@qq.com>
    Co-authored-by: Jinhang Su <171846802@qq.com>

---
+++ sdgx/metrics/single_table/base.py
@@ -0,0 +1,78 @@
+import pandas as pd
+
+from sdgx.log import logger
+
+
+class SingleTableMetric:
+    """SingleTableMetric
+
+    Metrics used to evaluate the quality of single table synthetic data.
+    """
+
+    upper_bound = None
+    lower_bound = None
+    metric_name = None
+    metadata = None
+
+    def __init__(self, metadata: dict) -> None:
+        """Initialization
+
+        Args:
+            metadata(dict): This parameter accepts a metadata description dict, which is used to describe the column description information of the table.
+        """
+        self.metadata = metadata
+        pass
+
+    @classmethod
+    def check_input(cls, real_data: pd.DataFrame, synthetic_data: pd.DataFrame):
+        """Input check for single table input.
+
+        Args:
+            real_data(pd.DataFrame): the real (original) data table.
+
+            synthetic_data(pd.DataFrame): the synthetic (generated) data table.
+        """
+        # should be pd.DataFrame
+        # Input parameter must not contain None value
+        if real_data is None or synthetic_data is None:
+            raise TypeError("Input contains None.")
+
+        # The data type should be same
+        if type(real_data) is not type(synthetic_data):
+            raise TypeError("Data type of real_data and synthetic data should be the same.")
+
+        # if type is pd.Series, return directly
+        if isinstance(real_data, pd.DataFrame):
+            return real_data, synthetic_data
+
+        # if type is not pd.Series or pd.DataFrame tranfer it to Series
+        try:
+            real_data = pd.DataFrame(real_data)
+            synthetic_data = pd.DataFrame(synthetic_data)
+            return real_data, synthetic_data
+        except Exception as e:
+            logger.error(f"An error occurred while converting to pd.DataFrame: {e}")
+
+        return None, None
+
+    def calculate(self, real_data: pd.DataFrame, synthetic_data: pd.DataFrame):
+        """Calculate the metric value between a real table and a synthetic table.
+
+        Args:
+            real_data(pd.DataFrame): the real (original) data table.
+
+            synthetic_data(pd.DataFrame): the synthetic (generated) data table.
+        """
+
+        raise NotImplementedError()
+
+    @classmethod
+    def check_output(raw_metric_value: float):
+        """Check the output value.
+
+        Args:
+            raw_metric_value (float):  the calculated raw value of the JSD metric.
+        """
+        raise NotImplementedError()
+
+    pass
