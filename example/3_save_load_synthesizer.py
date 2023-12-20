c727845a107cb40a071e591e8d3aa0bf641ed7c6
Wh1isper <9573586@qq.com>
2023-12-20 14:37:39 +0800

    Update comments

--- example/3_save_load_synthesizer.py
+++ example/3_save_load_synthesizer.py
@@ -1,8 +1,9 @@
-import os
+"""
+Example for fit and save a model, then load it for sampling.
+"""
+
 import time
 from pathlib import Path
-
-os.environ["SDGX_LOG_LEVEL"] = "DEBUG"
 
 from sdgx.data_connectors.csv_connector import CsvConnector
 from sdgx.models.ml.single_table.ctgan import CTGANSynthesizerModel
@@ -11,18 +12,25 @@ from sdgx.utils import download_demo_data
 
 _HERE = Path(__file__).parent
 
+# This will download demo data to ./dataset
 dataset_csv = download_demo_data()
+
+# Create data connector for csv file
 data_connector = CsvConnector(path=dataset_csv)
+
+# Initialize synthesizer, use CTGAN model
 synthesizer = Synthesizer(
     model=CTGANSynthesizerModel(epochs=1),  # For quick demo
     data_connector=data_connector,
 )
-synthesizer.fit()
 
+# Fit and save model
+synthesizer.fit()
 date = time.strftime("%Y%m%d-%H%M%S")
 save_dir = _HERE / f"./ctgan-{date}-model"
 synthesizer.save(save_dir)
 
+# Load model, then sample
 synthesizer = Synthesizer.load(save_dir, model=CTGANSynthesizerModel)
 sampled_data = synthesizer.sample(1000)
 print(sampled_data)
