10f017562c11b9f6883b7cebdd2520ea1fea7411
MoooCat <gyz@guoyunzhe.com>
2023-12-24 18:52:53 +0800

    Update relationship.py

--- sdgx/data_models/relationship.py
+++ sdgx/data_models/relationship.py
@@ -1,2 +1,28 @@
-class Relationship:
-    pass
+
+from pydantic import BaseModel
+
+class Relationship(BaseModel):
+    '''Relationship between tables 
+
+    For parent table, we don't need define primary key here.
+    The primary key is pre-defined in parent table's metadata.
+
+    Child table's foreign key should be defined here.
+    '''
+    
+    metadata_version: str = "1.0"
+    
+    # table names 
+    parent_table: str
+    child_table: str
+
+    # keys
+    child_table_foreign_key: str
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        if self.parent_table == self.child_table:
+            raise ValueError("child table and parent table cannot be the same")
+
+
