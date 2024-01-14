931f6ac233cc9d58adaeaa8fa182e172045c2350
sjh <171846802@qq.com>
2024-01-14 17:08:48 +0800

    using the fixture

--- tests/metrics/test_jsd.py
+++ tests/metrics/test_jsd.py
@@ -1,51 +1,40 @@
+from __future__ import annotations
+import random
+
 import numpy as np
 import pandas as pd
 import pytest
-
 from sdgx.metrics.column.jsd import JSD
 
+
 # 创建测试数据
-real_data_discrete = pd.DataFrame(
-    {
-        "col1": ["a", "b", "c", "d", "e"],
-        "col2": ["a", "b", "b", "b", "e"],
-    }
-)
-
-synthetic_data_discrete = pd.DataFrame(
-    {
-        "col1": ["a", "c", "d", "b", "b"],
-        "col2": ["a", "c", "a", "a", "e"],
-    }
-)
-
-real_data_cotinuous = pd.DataFrame(
-    {
-        "col1": [1, 1, 2, 2, 3],
-        "col2": [4, 4, 5, 5, 6],
-    }
-)
-
-synthetic_data_cotinuous = pd.DataFrame(
-    {
-        "col1": [1, 2, 2, 3, 3],
-        "col2": [4, 5, 5, 6, 6],
-    }
-)
-
-# 创建 JSD 实例
-jsd_instance = JSD()
+@pytest.fixture
+def dummy_data(dummy_single_table_path):
+    yield pd.read_csv(dummy_single_table_path)
 
 
-def test_jsd_discrete():
-    cols = ["col1", "col2"]
-    result = jsd_instance.calculate(
-        real_data_discrete, synthetic_data_discrete, cols, discrete=True
+@pytest.fixture
+def test_data():
+    role_set = ["admin", "user", "guest"]
+    df = pd.DataFrame(
+        {
+            "role": [random.choice(role_set) for _ in range(10)],
+            "feature_x": [random.random() for _ in range(10)]
+        }
     )
-    result1 = jsd_instance.calculate(real_data_discrete, real_data_discrete, cols, discrete=True)
-    result2 = jsd_instance.calculate(
-        synthetic_data_discrete, real_data_discrete, cols, discrete=True
-    )
+    return df
+
+
+@pytest.fixture
+def jsd_instance():
+    return JSD()
+
+
+def test_jsd_discrete(dummy_data, test_data, jsd_instance):
+    cols = ['role']
+    result = jsd_instance.calculate(dummy_data, test_data, cols, discrete=True)
+    result1 = jsd_instance.calculate(dummy_data, dummy_data, cols, discrete=True)
+    result2 = jsd_instance.calculate(test_data, dummy_data, cols, discrete=True)
 
     assert result >= 0
     assert result <= 1
@@ -53,17 +42,15 @@ def test_jsd_discrete():
     assert result2 == result
 
 
-def test_jsd_continuous():
-    cols = ["col1"]
-    result = jsd_instance.calculate(
-        real_data_cotinuous, synthetic_data_cotinuous, cols, discrete=False
-    )
-    result1 = jsd_instance.calculate(real_data_cotinuous, real_data_cotinuous, cols, discrete=False)
-    result2 = jsd_instance.calculate(
-        synthetic_data_cotinuous, real_data_cotinuous, cols, discrete=False
-    )
+def test_jsd_continuous(dummy_data,test_data, jsd_instance):
+    cols = ["feature_x"]
+    result = jsd_instance.calculate(dummy_data, test_data, cols, discrete=False)
+    result1 = jsd_instance.calculate(dummy_data, dummy_data, cols, discrete=False)
 
     assert result >= 0
     assert result <= 1
     assert result1 == 0
-    assert result2 == result
+
+
+if __name__ == "__main__":
+    pytest.main(["-vv", "-s", __file__])
