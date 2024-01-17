5ff18bf08dfec574039c6c33e595977e2a8a2171
MooooCat <gyz@guoyunzhe.com>
2024-01-17 11:08:04 +0800

    add inspect_level

--- sdgx/data_models/inspectors/datetime.py
+++ sdgx/data_models/inspectors/datetime.py
@@ -11,6 +11,14 @@ from sdgx.utils import ignore_warnings
 
 
 class DatetimeInspector(Inspector):
+
+    inspect_level = 2
+    '''
+    The inspect_level of DatetimeInspector is higher than DiscreteInspector.
+
+    Often, difficult-to-recognize date or datetime objects are also recognized as descrete types by DatetimeInspector, causing the column to be marked repeatedly.
+    '''
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.datetime_columns: set[str] = set()
