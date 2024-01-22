3223ca9654c7d1302bf047188850fb0e7d347ec7
MooooCat <gyz@guoyunzhe.com>
2024-01-22 10:41:46 +0800

    add _inspect_level

--- sdgx/data_models/inspectors/personal.py
+++ sdgx/data_models/inspectors/personal.py
@@ -5,24 +5,30 @@ from sdgx.data_models.inspectors.regex import RegexInspector
 class EmailInspector(RegexInspector):
     pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
     data_type_name = "email"
+    _inspect_level = 30
 
 
 class ChinaMainlandIDInspector(RegexInspector):
+    # this pattern may be not right
     pattern = r"(^[1-9]\\d{5}(18|19|20)\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]$)|"
 
     data_type_name = "china_mainland_id"
+    
+    _inspect_level = 30
+
 
 
 class ChinaMainlandMobilePhoneInspector(RegexInspector):
     pattern = r"^1[3-9]\d{9}$"
 
     data_type_name = "china_mainland_mobile_phone"
+    _inspect_level = 30
 
 
 @hookimpl
 def register(manager):
     manager.register("EmailInspector", EmailInspector)
 
-    manager.register("ChinaMainlandIDInspector", ChinaMainlandIDInspector)
+    # manager.register("ChinaMainlandIDInspector", ChinaMainlandIDInspector)
 
     manager.register("ChinaMainlandMobilePhoneInspector", ChinaMainlandMobilePhoneInspector)
