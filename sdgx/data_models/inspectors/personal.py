73fce1bca0d2cbe970fa570eb587e5e8badc25f8
MooooCat <gyz@guoyunzhe.com>
2024-01-23 17:57:15 +0800

    add personal info inspector

--- sdgx/data_models/inspectors/personal.py
+++ sdgx/data_models/inspectors/personal.py
@@ -3,6 +3,7 @@ from sdgx.data_models.inspectors.regex import RegexInspector
 
 
 class EmailInspector(RegexInspector):
+
     pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
 
     data_type_name = "email"
@@ -13,8 +14,8 @@ class EmailInspector(RegexInspector):
 
 
 class ChinaMainlandIDInspector(RegexInspector):
-    # NOTE this pattern may be not right
-    pattern = r"(^[1-9]\\d{5}(18|19|20)\\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\\d{3}[0-9Xx]$)|"
+
+    pattern = r"^[1-9]\d{5}(18|19|20)\d{2}((0[1-9])|(1[0-2]))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$"
 
     data_type_name = "china_mainland_id"
 
@@ -24,9 +25,37 @@ class ChinaMainlandIDInspector(RegexInspector):
 
 
 class ChinaMainlandMobilePhoneInspector(RegexInspector):
+
     pattern = r"^1[3-9]\d{9}$"
 
     data_type_name = "china_mainland_mobile_phone"
+
+    _inspect_level = 30
+
+    pii = True
+
+# 邮编
+class ChinaMainlandPostCode(RegexInspector):
+
+    pattern = r"^[0-9]{6}$"
+
+    match_percentage = 0.95
+    '''
+    Since zip codes and six-digit integers are the same, here we increase match_percentage to prevent some pure integer columns from being recognized.
+    '''
+
+    data_type_name = "china_mainland_postcode"
+
+    _inspect_level = 20
+
+    pii = False
+
+# 纳税人识别号
+class ChinaMainlandTaxID(RegexInspector):
+
+    pattern = r"^(?:(?P<province>[A-Z]{2})|\d{6})\d{2}(?P<type>[07])\d{6}(?P<check>[0-9A-Z])$"
+
+    data_type_name = "china_mainland_tax_ID"
 
     _inspect_level = 30
 
@@ -37,6 +66,10 @@ class ChinaMainlandMobilePhoneInspector(RegexInspector):
 def register(manager):
     manager.register("EmailInspector", EmailInspector)
 
-    # manager.register("ChinaMainlandIDInspector", ChinaMainlandIDInspector)
+    manager.register("ChinaMainlandIDInspector", ChinaMainlandIDInspector)
 
     manager.register("ChinaMainlandMobilePhoneInspector", ChinaMainlandMobilePhoneInspector)
+
+    manager.register("ChinaMainlandPostCode", ChinaMainlandPostCode)
+
+    manager.register("ChinaMainlandTaxID", ChinaMainlandTaxID)
