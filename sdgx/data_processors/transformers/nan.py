2eab25caf7ddec187bf266e2c4d5de078084285f
MooooCat <gyz@guoyunzhe.com>
2024-03-05 16:44:20 +0800

    intro NaN processor

---
+++ sdgx/data_processors/transformers/nan.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from pandas import DataFrame  
+from sdgx.data_models.metadata import Metadata  
+from sdgx.data_processors.transformers.base import Transformer 
+from sdgx.data_processors.extension import hookimpl
+from sdgx.utils import logger
+
+class NonValueTransformer(Transformer):
+    '''
+    Transformer class for handling missing values in data.
+
+    This Transformer is mainly used as a reference for Transformer to facilitate developers to quickly understand the role of Transformer.
+    '''
+
+    def fit(self, metadata: Metadata | None = None):
+        '''
+        Fit method for the transformer. 
+        
+        Does not require any action.
+        '''
+        logger.info("Fitting NonValueTransformer")
+
+        return 
+
+    def convert(self, raw_data: DataFrame) -> DataFrame:
+        '''
+        Convert method to handle missing values in the input data.
+        '''
+        logger.info("Converting data using NonValueTransformer")
+
+        return raw_data.fillna()  
+    
+    def reverse_convert(self, processed_data: DataFrame) -> DataFrame:
+        '''
+        Reverse_convert method for the transformer. 
+        
+        Does not require any action.
+        '''
+        return processed_data
+
+    pass
+
+@hookimpl
+def register(manager):
+    manager.register("NonValueTransformer", NonValueTransformer)
+
