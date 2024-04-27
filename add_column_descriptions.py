135ef4c24d16baad2f302cd24862d8b4f713293d
sam <sam3179@proton.me>
2024-04-27 15:09:11 -0400

    personal issue 142: added column details for datetime64 data type. column details start date and end date

--- add_column_descriptions.py
+++ add_column_descriptions.py
@@ -16,21 +16,25 @@ class AddColumnDescriptions:
             data_type = column_data.dtype
 
             descriptions[i] = (
-                f"column #{i}\ncolumn name: {column_name}\ncolumn data type: {data_type}"
+                f"column #{i}\ncolumn name: {column_name}\ncolumn data type: {data_type}\n"
             )
 
             if data_type == "int64":
                 min = column_data.min()
                 max = column_data.max()
                 mean = column_data.mean()
-                std_deviation = column_data.std
+                std_deviation = column_data.std()
                 descriptions[i] = descriptions[i] + (
                     f"min value: {min}\nmax value: {max}\nmean value: {mean}\n"
-                    f"standard deviation: {std_deviation}"
+                    f"standard deviation: {std_deviation}\n"
                 )
 
             elif data_type == "datetime64":
                 print("data type is datetime64")
+                start_date = column_data.min()
+                end_date = column_data.max
+
+                descriptions[i] = descriptions[i] + f"start date: {start_date}\nend date: {end_date}\n"
 
             # if column_data.dtype == 'object':
             #     unique_values = column_data.unique()
@@ -39,5 +43,6 @@ class AddColumnDescriptions:
             # elif column_data.dtype == 'int64':
             #     column_mean = column_data.mean()
             #     print(f"mean: {column_mean}")
+        print("")
         for element in descriptions:
             print(element)
