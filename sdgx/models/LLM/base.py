10a74bde469644f4cc6afa6735d9670cf787a382
sam <sam3179@proton.me>
2024-04-30 19:15:23 -0400

    personal issue 142: moved method contents to file where issue asks, instead of production file

--- sdgx/models/LLM/base.py
+++ sdgx/models/LLM/base.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from sdgx.exceptions import SynthesizerInitError
 from sdgx.models.base import SynthesizerModel
 from sdgx.utils import logger
@@ -86,14 +88,33 @@ class LLMBaseModel(SynthesizerModel):
         if self.use_dataloader == self.use_raw_data == True:
             raise SynthesizerInitError("Duplicate data access type found.")
 
-    def _form_columns_description(self):
-        """
-        We believe that giving information about a column helps improve data quality.
+    def _form_columns_description(self, sampled_data):
+        sampled_data_df = pd.DataFrame(sampled_data)
 
-        Currently, we leave this function to Good First Issue until March 2024, if unclaimed we will implement it quickly.
-        """
+        all_descriptions = ""
 
-        raise NotImplementedError
+        for i, (column_name, column_data) in enumerate(sampled_data_df.items()):
+            data_type = column_data.dtype
+
+            description = (
+                f"column #{i}\ncolumn name: {column_name}\ncolumn data type: {data_type}\n"
+            )
+
+            if data_type == "int64":
+                description += _add_int64_column_descriptions(column_data)
+
+            elif data_type == "datetime64":
+                description += _add_datetime64_column_descriptions(column_data)
+
+            elif data_type == "category":
+                description += _add_category_column_descriptions(column_data)
+
+            elif data_type == "object":
+                description += _add_object_column_descriptions(column_data)
+            all_descriptions += "\n"
+            all_descriptions += description
+
+        return all_descriptions
 
     def _form_message_with_offtable_features(self):
         """
@@ -121,3 +142,42 @@ class LLMBaseModel(SynthesizerModel):
         else:
             logger.info("No dataset_description given in current model.")
             return ""
+
+
+def _add_int64_column_descriptions(column_data):
+    min_value = column_data.min()
+    max_value = column_data.max()
+    mean_value = column_data.mean()
+    std_deviation = column_data.std()
+    description = (
+        f"min value: {min_value}\nmax value: {max_value}\nmean value: {mean_value}\n"
+        f"standard deviation: {std_deviation}\n"
+    )
+    return description
+
+
+def _add_datetime64_column_descriptions(column_data):
+    start_date = column_data.min()
+    end_date = column_data.max
+    description = f"start date: {start_date}\nend date: {end_date}\n"
+    return description
+
+
+def _add_category_column_descriptions(column_data):
+    all_categories = len(column_data.values())
+    unique_categories = column_data.unique()
+    description = (
+        f"number of all category values: {all_categories}\nnumber of "
+        f"unique category values: {unique_categories}\n"
+    )
+    return description
+
+
+def _add_object_column_descriptions(column_data):
+    all_objects = len(column_data)
+    unique_objects = len(column_data.unique())
+    description = (
+        f"number of all object values: {all_objects}\nnumber of "
+        f"unique object values: {unique_objects}\n"
+    )
+    return description
