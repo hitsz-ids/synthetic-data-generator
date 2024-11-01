14fe89c2d5f537874b3a5d16cfc72687f64f5ad0
Elvin <1487819688@qq.com>
2024-11-01 20:05:02 +0800

    BugFix: Parallel Data Processing

--- sdgx/models/components/optimize/sdv_ctgan/data_transformer.py
+++ sdgx/models/components/optimize/sdv_ctgan/data_transformer.py
@@ -169,12 +169,15 @@ class DataTransformer(object):
             else:
                 process = delayed(self._transform_discrete)(column_transform_info, data)
             processes.append(process)
-        try:
-            # For Future versions of joblib
-            p = Parallel(n_jobs=-1, return_as="generator_unordered")
-            logger.warning("Using generator_unordered in joblib, time to remove this exception!")
-        except ValueError:
-            p = Parallel(n_jobs=-1, return_as="generator")
+
+        # TODO This method has bug, which are deprecated. Time to find a better method instead.
+        # try:
+        #     # For Future versions of joblib
+        #     p = Parallel(n_jobs=-1, return_as="generator_unordered")
+        #     logger.warning("Using generator_unordered in joblib, time to remove this exception!")
+        # except ValueError:
+
+        p = Parallel(n_jobs=-1, return_as="generator")
 
         loader = NDArrayLoader()
         for ndarray in p(processes):
